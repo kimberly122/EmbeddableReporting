a9087c785994dd8bb05107d5a3b407d9ac6e643b
kimberly1295 <kmbrlpl12@gmail.com>
2016-03-13 21:39:33 +0800

    updated cloudant

--- src/main/java/servlet/CloudantUpload.java
+++ src/main/java/servlet/CloudantUpload.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
